2a08d7938c67b6572f82ea77b41049b85d3b2a93
D3ns0n <d3ns0n@d3ns0n.de>
2016-10-02 13:29:08 +0200

    add methods to encrypt end decrypt messages

--- caesar/caesar.py
+++ caesar/caesar.py
@@ -1,7 +1,24 @@
 from string import ascii_lowercase as lc, ascii_uppercase as uc
 
+rotation_step_enc = -3 # rotation step for encrypting messages
+
 def rot(rotation_step, letter):
     origin_alphabet = lc + uc 
     rotated_alphabet = lc[rotation_step:] + lc[:rotation_step] + uc[rotation_step:] + uc[:rotation_step] # create rotated alphabet
     index = origin_alphabet.index(letter) # determine index of letter in alphabet
-    return rotated_alphabet[index]
+    return rotated_alphabet[index] # return letter at index in rotated alphabet
+    
+def encrypt(message):
+    encrypted_message = ''
+    for letter in message:
+        encrypted_message += rot(rotation_step_enc, letter)
+    return encrypted_message
+    
+def decrypt(message):
+    decrypted_message = ''
+    for letter in message:
+        decrypted_message += rot(-rotation_step_enc, letter)
+    return decrypted_message
+    
+    
+    
--- caesar/test_caesar.py
+++ caesar/test_caesar.py
@@ -7,3 +7,9 @@ class TestStringMethods(unittest.TestCase):
         rotation_step = -3
         self.assertEqual(caesar.rot(rotation_step, 'D'), 'A')
         self.assertEqual(caesar.rot(rotation_step, 'C'), 'Z')
+        
+    def test_encrypt(self):
+        self.assertEqual(caesar.encrypt('HELLO'), 'EBIIL')
+        
+    def test_decrypt(self):
+        self.assertEqual(caesar.decrypt('EBIIL'), 'HELLO')
