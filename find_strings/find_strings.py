4504983e5b55551186d59d3d63478a1512a3c9ad
D3ns0n <d3ns0n@d3ns0n.de>
2016-10-05 20:36:08 +0200

    add main method to find_strings with basic error handling

--- find_strings/find_strings.py
+++ find_strings/find_strings.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import sys
 
 
 def permutate(string):
@@ -14,3 +15,18 @@ def permutate(string):
         for p in permutate(rest):
             result.append(first + p)
     return result
+
+
+def print_usage():
+    print('Usage: find_strings.py <number> \n')
+    print('Number: an integer to encode as character \n')
+    print('Example: find_strings.py 123')
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:  # if less than 2 arguments given on command line
+        print_usage()
+    else:
+        number = sys.argv[1]
+        for permutation in permutate(number):
+            print(permutation)
