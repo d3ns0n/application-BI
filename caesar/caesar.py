a9ac7e528603ee2ed1ea9b31f94b2cb64c59bf2a
D3ns0n <d3ns0n@d3ns0n.de>
2016-10-15 20:21:36 +0200

    format code in caesar.py and test_caesar.py

--- caesar/caesar.py
+++ caesar/caesar.py
@@ -1,50 +1,55 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 from string import ascii_lowercase as lc, ascii_uppercase as uc
 import sys
 
-rotation_step_enc = -3 # rotation step for encrypting messages
+rotation_step_enc = -3  # rotation step for encrypting messages
+
 
 def rot(rotation_step, letter):
     origin_alphabet = lc + uc
     if letter not in origin_alphabet:
-        return letter 
-    rotated_alphabet = lc[rotation_step:] + lc[:rotation_step] + uc[rotation_step:] + uc[:rotation_step] # create rotated alphabet
-    index = origin_alphabet.index(letter) # determine index of letter in alphabet
-    return rotated_alphabet[index] # return letter at index in rotated alphabet
-    
+        return letter
+    rotated_alphabet = lc[rotation_step:] + lc[:rotation_step] + uc[rotation_step:] + uc[
+                                                                                      :rotation_step]  # create rotated alphabet
+    index = origin_alphabet.index(letter)  # determine index of letter in alphabet
+    return rotated_alphabet[index]  # return letter at index in rotated alphabet
+
+
 def encrypt(message):
     encrypted_message = ''
     for letter in message:
-        encrypted_message += rot(rotation_step_enc, letter) # rotate to encrypt message
+        encrypted_message += rot(rotation_step_enc, letter)  # rotate to encrypt message
     return encrypted_message
-    
+
+
 def decrypt(message):
     decrypted_message = ''
     for letter in message:
-        decrypted_message += rot(-rotation_step_enc, letter) # rotate in opposite direction for decryption
+        decrypted_message += rot(-rotation_step_enc, letter)  # rotate in opposite direction for decryption
     return decrypted_message
-    
+
+
 def print_usage():
     print('Usage: caesar.py <command> <message> \n')
     print('Commands: decrypt, encrypt')
     print('Message: string to en- / decrypt \n')
     print('Example: caesar.py encrypt "HELLO"')
-    
+
+
 if __name__ == '__main__':
-    if len(sys.argv) != 3: # if less than 2 arguments given on command line
+    if len(sys.argv) != 3:  # if less than 2 arguments given on command line
         print_usage()
-        
-    else:     
+
+    else:
         mode = sys.argv[1]
         message = sys.argv[2]
-        
+
         if mode == 'encrypt':
             print(encrypt(message))
-            
+
         elif mode == 'decrypt':
             print(decrypt(message))
-        
-        else: # if given mode is neither encrypt nor decrypt
+
+        else:  # if given mode is neither encrypt nor decrypt
             print_usage()
--- caesar/test_caesar.py
+++ caesar/test_caesar.py
@@ -1,23 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import unittest, caesar
 
-import unittest
-import caesar
 
 class TestStringMethods(unittest.TestCase):
-    
     def test_rot(self):
         rotation_step = -3
         self.assertEqual(caesar.rot(rotation_step, 'D'), 'A')
         self.assertEqual(caesar.rot(rotation_step, 'C'), 'Z')
         self.assertEqual(caesar.rot(rotation_step, '!'), '!')
         self.assertEqual(caesar.rot(rotation_step, ','), ',')
-        
+
     def test_encrypt(self):
         self.assertEqual(caesar.encrypt('HELLO'), 'EBIIL')
         self.assertEqual(caesar.encrypt('1!.;$&ä'), '1!.;$&ä')
         self.assertEqual(caesar.encrypt('HELLO.'), 'EBIIL.')
-        
+
     def test_decrypt(self):
         self.assertEqual(caesar.decrypt('EBIIL'), 'HELLO')
         self.assertEqual(caesar.decrypt('1!.;$&ä'), '1!.;$&ä')
